2fb9bf85fbbb932f81428e21398c50f7771364b3
linke5 <82665891+linke5@users.noreply.github.com>
2022-05-03 18:47:25 +0800

    Add files via upload

---
+++ Deploy-New.sh
@@ -0,0 +1,119 @@
+
+echo " ========================================================= "
+echo " \                Deploy-New.sh               / "
+echo " \          一键拉取XrayR配置文件|并配置          / "
+echo " \ Old Renewed By Surplus|New Renewed By Ling / "
+echo " ========================================================= "
+
+rm -rf /etc/systemd/system/XrayR.service
+wget https://cdn.jsdelivr.net/gh/linke5/cdn-repository/lilicloud/XrayR.service
+mv XrayR.service /etc/systemd/system
+systemctl daemon-reload
+
+rm -rf /etc/XrayR/config.yml
+wget https://fastly.jsdelivr.net/gh/linke5/cdn-repository/lilicloud/config.yml
+mv config.yml /etc/XrayR/
+
+rm -rf /etc/XrayR/cert
+mkdir /etc/XrayR/cert
+wget https://cdn.jsdelivr.net/gh/linke5/cdn-repository/ssl/licom-ga/fullchain.crt
+mv fullchain.crt /etc/XrayR/cert
+wget https://cdn.jsdelivr.net/gh/linke5/cdn-repository/ssl/licom-ga/private.pem
+mv private.pem /etc/XrayR/cert
+
+read -p "节点idtr:" idtr
+read -p "节点idss:" idss
+read -p "节点设备限制数:" drmun
+cat<<EOF>/etc/XrayR/config.yml
+Log:
+  Level: debug # Log level: none, error, warning, info, debug 
+  AccessPath: # /etc/XrayR/access.Log
+  ErrorPath: # /etc/XrayR/error.log
+DnsConfigPath: # /etc/XrayR/dns.json # Path to dns config, check https://xtls.github.io/config/base/dns/ for help
+RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/base/route/ for help
+OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/base/outbound/ for help
+ConnetionConfig:
+  Handshake: 4 # Handshake time limit, Second
+  ConnIdle: 30 # Connection idle time limit, Second
+  UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
+  DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
+  BufferSize: 64 # The internal cache size of each connection, kB 
+Nodes:
+  -
+    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, , Proxypanel
+    ApiConfig:
+      ApiHost: "http://up-xrayr.csflorence.net"
+      ApiKey: "wwwwww123123123123"
+      NodeID: $idtr
+      NodeType: Trojan # Node type: V2ray, Shadowsocks, Trojan, Shadowsocks-Plugin
+      Timeout: 120 # Timeout for the api request
+      EnableVless: false # Enable Vless for V2ray Type
+      EnableXTLS: false # Enable XTLS for V2ray and Trojan
+      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
+      DeviceLimit: $drmun # Local settings will replace remote settings, 0 means disable
+      RuleListPath: # ./rulelist Path to local rulelist file
+    ControllerConfig:
+      ListenIP: 0.0.0.0 # IP address you want to listen
+      SendIP: 0.0.0.0 # IP address you want to send pacakage
+      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
+      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
+      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
+      EnableProxyProtocol: false # Only works for WebSocket and TCP
+      EnableFallback: false # Only support for Trojan and Vless
+      FallBackConfigs:  # Support multiple fallbacks
+        -
+          SNI: # TLS SNI(Server Name Indication), Empty for any
+          Path: # HTTP PATH, Empty for any
+          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
+          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
+      CertConfig:
+        CertMode: file # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
+        CertDomain: "*.licom.ga" # Domain to cert
+        CertFile: /etc/XrayR/cert/fullchain.crt # Provided if the CertMode is file
+        KeyFile: /etc/XrayR/cert/private.pem
+        Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
+        Email: test@me.com
+        DNSEnv: # DNS ENV option used by DNS provider
+          ALICLOUD_ACCESS_KEY: aaa
+          ALICLOUD_SECRET_KEY: bbb
+  -
+    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, , Proxypanel
+    ApiConfig:
+      ApiHost: "http://up-xrayr.csflorence.net"
+      ApiKey: "wwwwww123123123123"
+      NodeID: $idss
+      NodeType: Shadowsocks # Node type: V2ray, Shadowsocks, Trojan, Shadowsocks-Plugin
+      Timeout: 120 # Timeout for the api request
+      EnableVless: false # Enable Vless for V2ray Type
+      EnableXTLS: false # Enable XTLS for V2ray and Trojan
+      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
+      DeviceLimit: $drmun # Local settings will replace remote settings, 0 means disable
+      RuleListPath: # ./rulelist Path to local rulelist file
+    ControllerConfig:
+      ListenIP: 0.0.0.0 # IP address you want to listen
+      SendIP: 0.0.0.0 # IP address you want to send pacakage
+      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
+      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
+      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
+      EnableProxyProtocol: false # Only works for WebSocket and TCP
+      EnableFallback: false # Only support for Trojan and Vless
+      FallBackConfigs:  # Support multiple fallbacks
+        -
+          SNI: # TLS SNI(Server Name Indication), Empty for any
+          Path: # HTTP PATH, Empty for any
+          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
+          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
+      CertConfig:
+        CertMode: file # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
+        CertDomain: "*.licom.ga" # Domain to cert
+        CertFile: /etc/XrayR/cert/fullchain.crt # Provided if the CertMode is file
+        KeyFile: /etc/XrayR/cert/private.pem
+        Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
+        Email: test@me.com
+        DNSEnv: # DNS ENV option used by DNS provider
+          ALICLOUD_ACCESS_KEY: aaa
+          ALICLOUD_SECRET_KEY: bbb
+          EOF
+XrayR restart
+
+echo "请忽略上句红色的字"
